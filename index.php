9c1df2e4ff637a19a60c5dca0df2a07c623d57ab
Julien Faby <julienfaby@gmail.com>
2023-09-19 02:11:15 +0200

    Ajout de la page index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page d'accueil</title>
+</head>
+<body>
+    <!-- Le contenu de la page d'accueil -->
+    <?php
+    // Inclure le contenu de la page "accueil.php"
+    include('Page-Accueil/accueil.php');
+    ?>
+
+    <!-- Le reste de votre contenu HTML -->
+    <p>Autre contenu de la page d'accueil.</p>
+</body>
+</html>
