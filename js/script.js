39b766a9541ff767478da722dcbf994e2c49fd6c
Julien Faby <julienfaby@gmail.com>
2023-12-11 10:29:37 +0100

    Mise à jour filtres

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const modalContainer = document.createElement('div');
     modalContainer.classList.add('modal-container');
     document.body.appendChild(modalContainer);
-    
+
     const container = document.getElementById("cartes-container");
 
-    //Requête XMLHttpRequest pour récupérer les données des véhicules
+    // Requête XMLHttpRequest pour récupérer les données des véhicules
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "recupInfoCar.php", true);
     xhr.onload = function () {
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 carte.appendChild(bouton);
 
                 bouton.addEventListener('click', function () {
-                    afficherDetails(i); 
+                    afficherDetails(i);
                 });
 
                 container.appendChild(carte);
@@ -51,20 +51,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Fonction pour afficher les détails du véhicule en tant que modale
     function afficherDetails(index) {
-        const modalContainer = document.querySelector('.modal-container'); // Obtenir l'élément modal-container
-    
-        modalContainer.innerHTML = ''; // Efface le contenu précédent de la modale
+        const modalContainer = document.querySelector('.modal-container');
+
+        modalContainer.innerHTML = '';
         const modalContent = document.createElement('div');
         modalContent.classList.add('modal-content');
-         
 
-        //Nouvelle requête XMLHttpRequest pour récupérer les détails du véhicule
+        // Nouvelle requête XMLHttpRequest pour récupérer les détails du véhicule
         const modalXHR = new XMLHttpRequest();
         modalXHR.open("GET", `recupInfoModale.php?index=${index}`, true);
         modalXHR.onload = function () {
             if (modalXHR.status === 200) {
                 const modalData = JSON.parse(modalXHR.responseText);
-    
+
                 modalData.forEach(item => {
                     const modalText = document.createElement('p');
                     modalText.innerHTML = `
@@ -74,15 +73,15 @@ document.addEventListener("DOMContentLoaded", function () {
                         Puissance : ${item.puissance}<br>
                         Transmission : ${item.transmission}<br>`;
                     modalContent.appendChild(modalText);
-    
+
                     const closeButton = document.createElement('span');
                     closeButton.classList.add('close');
                     closeButton.textContent = 'X';
                     modalContent.appendChild(closeButton);
-    
+
                     modalContainer.appendChild(modalContent);
                     modalContainer.style.display = 'block';
-    
+
                     const photosGalerie1 = item.photos_galerie;
                     if (photosGalerie1) {
                         const photoUrls = photosGalerie1.split(' ');
@@ -99,16 +98,13 @@ document.addEventListener("DOMContentLoaded", function () {
                     contactButtonInner.textContent = 'Formulaire de contact';
                     contactButton.appendChild(contactButtonInner);
                     modalContent.appendChild(contactButton);
-    
+
                     closeButton.addEventListener('click', function () {
                         modalContainer.style.display = 'none';
                     });
 
                     modalContainer.appendChild(modalContent);
                     modalContainer.style.display = 'block';
-
-
-
                 });
             } else {
                 console.error("Erreur lors de la récupération des données pour la modale");
@@ -116,69 +112,85 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         modalXHR.send();
     }
-});
 
-//filtre véhicules
-// Écouteurs d'événements pour les éléments de filtrage
-const yearFilter = document.getElementById("year-filter");
-const priceFilter = document.getElementById("price-filter");
-const mileageFilter = document.getElementById("mileage-filter");
+    // Filtre véhicules
+    // Écouteurs d'événements pour les éléments de filtrage
+    const yearFilter = document.getElementById("year-filter");
+    const priceFilter = document.getElementById("price-filter");
+    const mileageFilter = document.getElementById("mileage-filter");
 
-yearFilter.addEventListener("change", applyFilters);
-priceFilter.addEventListener("change", applyFilters);
-mileageFilter.addEventListener("change", applyFilters);
+    yearFilter.addEventListener("change", applyFilters);
+    priceFilter.addEventListener("change", applyFilters);
+    mileageFilter.addEventListener("change", applyFilters);
 
-const filterButton = document.getElementById("filter-button");
-filterButton.addEventListener("click", applyFilters);
+    const filterButton = document.getElementById("filter-button");
+    filterButton.addEventListener("click", applyFilters);
 
-// Fonction pour appliquer les filtres
-function applyFilters() {
-    const selectedYear = yearFilter.value;
-    const selectedPrice = priceFilter.value;
-    const selectedMileage = mileageFilter.value;
+    // Fonction pour appliquer les filtres
+    function applyFilters() {
+        const selectedYear = yearFilter.value;
+        const selectedPrice = priceFilter.value;
+        const selectedMileage = mileageFilter.value;
 
-    // Effectuer la requête XMLHttpRequest avec les paramètres de filtrage
-    const filteredXHR = new XMLHttpRequest();
-    const url = `filtre.php?year=${selectedYear}&price=${selectedPrice}&mileage=${selectedMileage}`;
+        // Effectuer la requête XMLHttpRequest avec les paramètres de filtrage
+        const filteredXHR = new XMLHttpRequest();
+        const url = `filtre.php?year=${selectedYear}&price=${selectedPrice}&mileage=${selectedMileage}`;
 
-    filteredXHR.open("GET", url, true);
-    filteredXHR.onload = function () {
-        if (filteredXHR.status === 200) {
-            const filteredData = JSON.parse(filteredXHR.responseText);
+        filteredXHR.open("GET", url, true);
+        filteredXHR.onload = function () {
+            if (filteredXHR.status === 200) {
+                const filteredData = JSON.parse(filteredXHR.responseText);
 
-            // Mettre à jour l'affichage avec les données filtrées
-            const cartesContainer = document.getElementById("cartes-container");
-            cartesContainer.innerHTML = ""; // Effacer le contenu actuel
+                // Mettre à jour l'affichage avec les données filtrées
+                updateCards(filteredData);
 
-            // Boucle à travers les données filtrées et créer les éléments de carte correspondants
-            filteredData.forEach(item => {
-                const carte = document.createElement("div");
-                carte.classList.add("carte");
+            } else {
+                console.error("Erreur lors de la récupération des données filtrées");
+            }
+        };
 
-                const image = document.createElement("img");
-                image.src = item.photos;
-                carte.appendChild(image);
+        filteredXHR.send();
+    }
 
-                const titre = document.createElement("h3");
-                titre.textContent = item.modele;
-                carte.appendChild(titre);
+    // Fonction pour mettre à jour les cartes
+    function updateCards(data) {
+        const cartesContainer = document.getElementById("cartes-container");
+        // Effacer le contenu actuel
+        cartesContainer.innerHTML = "";
 
-                const description = document.createElement("p");
-                description.innerHTML = `
-                    Année : ${item.annee}<br>
-                    Énergie : ${item.energie}<br>
-                    Kilométrage : ${item.kilometrage} km<br>
-                    Prix : ${item.prix} €`;
-                carte.appendChild(description);
+        // Boucle à travers les données filtrées et créer les éléments de carte correspondants
+        data.forEach((item, i) => {
+            const carte = document.createElement("div");
+            carte.classList.add("carte");
 
-                cartesContainer.appendChild(carte);
+            const image = document.createElement("img");
+            image.src = item.photos;
+            carte.appendChild(image);
+
+            const titre = document.createElement("h3");
+            titre.textContent = item.modele;
+            carte.appendChild(titre);
+
+            const description = document.createElement("p");
+            description.innerHTML = `
+                Année : ${item.annee}<br>
+                Énergie : ${item.energie}<br>
+                Kilométrage : ${item.kilometrage} km<br>
+                Prix : ${item.prix} €`;
+            carte.appendChild(description);
+
+            // Créer le bouton "Détails"
+            const detailsButton = document.createElement("button");
+            detailsButton.textContent = "Détails";
+            detailsButton.addEventListener("click", function () {
+                // Logique pour gérer le clic sur le bouton "Détails"
+                console.log("Détails clicked for item:", item);
+                afficherDetails(i);
             });
+            carte.appendChild(detailsButton);
 
-        } else {
-            console.error("Erreur lors de la récupération des données filtrées");
-        }
-    };
-    
-    filteredXHR.send();
-}
-
+            // Ajouter la carte au conteneur
+            cartesContainer.appendChild(carte);
+        });
+    }
+});
