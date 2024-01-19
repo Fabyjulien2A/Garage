8b5de323a06a23ba4ee15e64c3789266a7e3b03b
Julien Faby <julienfaby@gmail.com>
2024-01-19 21:12:25 +0100

    mise à jour

--- Page-nos-vehicules/nosVehicules.php
+++ Page-nos-vehicules/nosVehicules.php
@@ -41,7 +41,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/bootstrap.css">   
     <link rel="stylesheet" href="../css/nosVehicules.css">
     <title>ECF GARAGE</title>
 </head>
